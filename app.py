8b08a93b0c53cbda334f1d004983d54a2a39f7fc
22236bhs <22236@burnside.school.nz>
2024-04-15 15:20:00 +1200

    tested database

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello, World")
+import sqlite3
+
+DATABASE = "fighters.db"
+
+with sqlite3.connect(DATABASE) as db:
+    cursor = db.cursor()
+    sql = "SELECT * FROM fighter;"
+    cursor.execute(sql)
+    results = cursor.fetchall()
+    for data in results:
+        print(data)
